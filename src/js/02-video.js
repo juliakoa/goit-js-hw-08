954638fb946c02155893d1c4b4c0fa9295f3722c
juliakoa <julisia199706@gmail.com>
2023-05-28 19:18:26 +0200

    video update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    const currentTime = event.seconds;
+
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime)
+    );
+  }, 1000)
+);
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime !== null) {
+    player.setCurrentTime(JSON.parse(currentTime));
+  }
+}
+
+window.addEventListener('load', setCurrentTime);
